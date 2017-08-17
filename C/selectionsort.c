05368e4694ab2342ea1cbec8eb68a203d91d20bf
cherneydh <31091775+cherneydh@users.noreply.github.com>
2017-08-17 00:20:04 -0700

    Updated name

--- C/selectionsort.c
+++ C/selectionsort.c
@@ -1,7 +1,7 @@
 /*
  * selectionsort.c
  * 
- * Copyright 2017 daniel <daniel@newmaker>
+ * Copyright 2017 Daniel @cherneydh
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
